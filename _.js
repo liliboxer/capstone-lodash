9cce34fd6eee51e241b9e5bb7e864cd0b4f3f663
Lili Boxer <ninaliboxer@gmail.com>
2018-12-03 08:11:55 -0800

    clamp method

---
+++ _.js
@@ -0,0 +1,27 @@
+const _ = {
+
+/////////////////////////////////////////////
+  clamp (number, lowerBound, upperBound) {
+    if (number < lowerBound) {
+      return lowerBound;
+    } else if (number > upperBound) {
+      return upperBound;
+    } else {
+      return number;
+    };
+  },
+/////////////////////////////////////////////
+  inRange () {
+
+  },
+
+
+
+
+
+};
+
+
+
+// Do not write or modify code below this line.
+module.exports = _;
